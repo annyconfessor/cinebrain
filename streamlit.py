7954d801c9c5687af9d4ec1f7c02b550e204cfcf
annyconfessor <anny.confessor@gmail.com>
2025-04-09 08:56:32 -0300

    fix: Title and Subtitle erros on Streamlit page

---
+++ streamlit.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import torch
+from model import RecommendedMovies
+from utils import search_movie, get_movie_details, process_input
+from PIL import Image
+import requests
+from io import BytesIO
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = RecommendedMovies(input_size=14)
+model.load_state_dict(torch.load("model.pth", map_location=device))
+model.eval()
+
+st.set_page_config(page_title="CineBrain 🎥", layout="centered")
+
+st.title("🎬 Previsor de Notas de Filmes")
+st.write("Digite o nome de um filme e veja qual nota o modelo prevê!")
+
+movie_name = st.text_input("Nome do filme: ")
+
+if movie_name:
+  movie_data = search_movie(movie_name)
+  details = get_movie_details(movie_data["id"])
+
+  if details["title"]:
+    st.subheader(details["title"])
+  else:
+    st.warning("No title found in details.")
+
+  if details:
+    st.subheader(details["overview"])
+    st.write("📊 Popularidade:", details["popularity"])
+    st.write("🎞️ Gêneros:", ", ".join(g["name"] for g in details["genres"]))
+    genres_ids = (g["id"] for g in details["genres"])
+
+    input_tensor = process_input(genres_ids, details["popularity"])
+    input_tensor = input_tensor.to(device)
+
+    with torch.no_grad():
+      output = model(input_tensor)
+      predicted_rating = output.item()
+
+    st.success(f"⭐ Nota prevista: **{predicted_rating:.2f} / 10**")
+
+else: st.warning("❌ Filme não encontrado. Tente outro nome.")
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
-import torch
 import requests
+import torch
 from config import API_KEY
-from model import RecommendedMovies
 
 genre_map = {
   28: 'Ação',
@@ -49,36 +48,3 @@ def process_input(genres_ids, popularity):
     # esse unsqueeze é para simular um batch
     torchx = torch.tensor(genre_vector + [popularity], dtype=torch.float32).unsqueeze(0)
     return torchx
-
-movie_name = input("Entre com o nome do filme: ")
-movie_data = search_movie(movie_name)
-
-if movie_data:
-   details = get_movie_details(movie_data["id"])
-   genres = []
-   for g in details.get("genres", []):
-      genres.append(g["id"])
-  #  genres = [g["id"] for g in details.get("genres", [])]
-   popularity = details.get("popularity", 0.0)
-   print(f"Gêneros: {genres} | Popularidade: {popularity:.2f}")
-
-   input_tensor = process_input(genres, popularity)
-
-  # De maneira geral, esse bloco de código pega um modelo vazio e preenche com modelos treinados
-   model = RecommendedMovies(input_size=14)
-  #  carrega e injeta o valor de model.pth no modelo criado, é tipo um rest do js
-  # o modelo onde o dado vai ser injetado tem que ser do mesmo tamanho que o dado carregado
-   model.load_state_dict(torch.load("model.pth")) 
-  #  entra no modo de avaliacao/inferencia
-   model.eval()
-
-  # para informar que nao estamos treinando o modelo
-   with torch.no_grad():
-      predicted_rating = model(input_tensor).item()
-      if predicted_rating:
-         print(f"Nota prevista para '{movie_name}': {predicted_rating:.2f}")
-      else:
-         print("Filme não encontrado")
-
-process_input([28,35], 5.0)
-
