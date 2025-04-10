671103be43e8013a238f592fa03bc29eccefde02
annyconfessor <anny.confessor@gmail.com>
2025-04-10 08:41:15 -0300

    fix: info and warning

--- streamlit.py
+++ streamlit.py
@@ -13,33 +13,33 @@ model.eval()
 
 st.set_page_config(page_title="CineBrain 🎥", layout="centered")
 
-st.title("🎬 Previsor de Notas de Filmes")
+st.title("🎬 Previsor de notas de filmes")
 st.write("Digite o nome de um filme e veja qual nota o modelo prevê!")
 
 movie_name = st.text_input("Nome do filme: ")
 
-if movie_name:
+if movie_name: 
   movie_data = search_movie(movie_name)
-  details = get_movie_details(movie_data["id"])
+  
+  if movie_data:
+    details = get_movie_details(movie_data["id"])
 
-  if details["title"]:
-    st.subheader(details["title"])
-  else:
-    st.warning("No title found in details.")
+    if details:
+      st.subheader(details["title"])
+      st.markdown(f"**Sinopse:** {details['overview']}")
+      st.write("📊 Popularidade:", details["popularity"])
+      st.write("🎞️ Gêneros:", ", ".join(g["name"] for g in details["genres"]))
+      genres_ids = (g["id"] for g in details["genres"])
 
-  if details:
-    st.subheader(details["overview"])
-    st.write("📊 Popularidade:", details["popularity"])
-    st.write("🎞️ Gêneros:", ", ".join(g["name"] for g in details["genres"]))
-    genres_ids = (g["id"] for g in details["genres"])
+      input_tensor = process_input(genres_ids, details["popularity"])
+      input_tensor = input_tensor.to(device)
 
-    input_tensor = process_input(genres_ids, details["popularity"])
-    input_tensor = input_tensor.to(device)
-
-    with torch.no_grad():
-      output = model(input_tensor)
-      predicted_rating = output.item()
+      with torch.no_grad():
+        output = model(input_tensor)
+        predicted_rating = output.item()
 
     st.success(f"⭐ Nota prevista: **{predicted_rating:.2f} / 10**")
+  
+  else: st.warning("Filme não encontrado.")
 
-else: st.warning("❌ Filme não encontrado. Tente outro nome.")
+else: st.info("Digite o nome do filme")
